241710f074c4cd3d22bf3eaea391bd6f1f54ba4a
Chase Roohms <chaseroohms@gmail.com>
2026-01-13 21:42:21 -0600

    fix: Standardize constant names to uppercase in RulesDatabase for consistency

--- src/main/java/Database/RulesDatabase.java
+++ src/main/java/Database/RulesDatabase.java
@@ -7,19 +7,19 @@ import net.dv8tion.jda.api.EmbedBuilder;
 public class RulesDatabase {
     /*Cover Rules                                                                               */
     /*==========================================================================================*/
-    public final static String halfCover ="""
+    public final static String HALF_COVER ="""
             A target has half cover if an obstacle blocks at least half of its body. The obstacle might be a low wall, a large piece of furniture, a narrow tree trunk, or a creature, whether that creature is an enemy or a friend.
             - +2 bonus to AC
             - +2 bonus to Dexterity saving throws
             """;
 
-    public final static String threeQuartersCover ="""
+    public final static String THREE_QUARTERS_COVER ="""
             A target has three-quarters cover if about three-quarters of it is covered by an obstacle. The obstacle might be a portcullis, an arrow slit, or a thick tree trunk.
             - +5 bonus to AC
             - +5 bonus to Dexterity saving throws
             """;
 
-    public final static String fullCover ="""
+    public final static String FULL_COVER ="""
             A target has total cover if it is completely concealed by an obstacle.
             - Can't be targeted directly by an attack or a spell
             - Some spells can reach such a target by including it in an area of effect
@@ -27,48 +27,48 @@ public class RulesDatabase {
 
     /*Condition Rules                                                                           */
     /*==========================================================================================*/
-    public final static String blinded ="""
+    public final static String BLINDED ="""
             - A blinded creature can't see and automatically fails any ability check that requires sight.
             - Attack rolls against the creature have advantage, and the creature's attack rolls have disadvantage.
             """;
 
-    public final static String  charmed ="""
+    public final static String  CHARMED ="""
             - A charmed creature can't attack the charmer or target the charmer with harmful abilities or magical effects.
             - The charmer has advantage on any ability check to interact socially with the creature.
             """;
 
-    public final static String  deafened ="""
+    public final static String  DEAFENED ="""
             - A deafened creature can't hear and automatically fails any ability check that requires hearing.
             """;
 
-    public final static String  frightened ="""
+    public final static String  FRIGHTENED ="""
             - A frightened creature has disadvantage on ability checks and attack rolls while the source of its fear is within line of sight.
             - The creature can't willingly move closer to the source of its fear.
             """;
 
-    public final static String  grappled ="""
+    public final static String  GRAPPLED ="""
             - A grappled creature's speed becomes 0, and it can't benefit from any bonus to its speed.
             - The condition ends if the grappler is incapacitated.
             - The condition also ends if an effect removes the grappled creature from the reach of the grappler or grappling effect, such as when a creature is hurled away by the thunderwave spell.
             """;
 
-    public final static String  incapacitated ="""
+    public final static String  INCAPACITATED ="""
             - An incapacitated creature can't take actions or reactions.
             """;
 
-    public final static String  invisible ="""
+    public final static String  INVISIBLE ="""
             - An invisible creature is impossible to see without the aid of magic or a special sense. For the purpose of hiding, the creature is heavily obscured. The creature's location can be detected by any noise it makes or any tracks it leaves.
             - Attack rolls against the creature have disadvantage, and the creature's attack rolls have advantage.
             """;
 
-    public final static String  paralyzed ="""
+    public final static String  PARALYZED ="""
             - A paralyzed creature is incapacitated and can't move or speak.
             - The creature automatically fails Strength and Dexterity saving throws.
             - Attack rolls against the creature have advantage.
             - Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.
             """;
 
-    public final static String  petrified ="""
+    public final static String  PETRIFIED ="""
             - A petrified creature is transformed, along with any nonmagical object it is wearing or carrying, into a solid inanimate substance (usually stone). Its weight increases by a factor of ten, and it ceases aging.
             - The creature is incapacitated, can't move or speak, and is unaware of its surroundings.
             - Attack rolls against the creature have advantage.
@@ -77,29 +77,29 @@ public class RulesDatabase {
             - The creature is immune to poison and disease, although a poison or disease already in its system is suspended, not neutralized.
             """;
 
-    public final static String  poisoned ="""
+    public final static String  POISONED ="""
             - A poisoned creature has disadvantage on attack rolls and ability checks.
             """;
 
-    public final static String  prone ="""
+    public final static String  PRONE ="""
             - A prone creature's only movement option is to crawl, unless it stands up and thereby ends the condition.
             - The creature has disadvantage on attack rolls.
             - An attack roll against the creature has advantage if the attacker is within 5 feet of the creature. Otherwise, the attack roll has disadvantage.
             """;
 
-    public final static String  restrained ="""
+    public final static String  RESTRAINED ="""
             - A restrained creature's speed becomes 0, and it can't benefit from any bonus to its speed.
             - Attack rolls against the creature have advantage, and the creature's attack rolls have disadvantage.
             - The creature has disadvantage on Dexterity saving throws.
             """;
 
-    public final static String  stunned ="""
+    public final static String  STUNNED ="""
             - A stunned creature is incapacitated (see the condition), can't move, and can speak only falteringly.
             - The creature automatically fails Strength and Dexterity saving throws.
             - Attack rolls against the creature have advantage.
             """;
 
-    public final static String  unconscious ="""
+    public final static String  UNCONSCIOUS ="""
             - An unconscious creature is incapacitated (see the condition), can't move or speak, and is unaware of its surroundings
             - The creature drops whatever it's holding and falls prone.
             - The creature automatically fails Strength and Dexterity saving throws.
@@ -107,7 +107,7 @@ public class RulesDatabase {
             - Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.
             """;
 
-    public final static String  exhaustion ="""
+    public final static String  EXHAUSTION ="""
             - **1:** Disadvantage on ability checks
             - **2:** Speed halved
             - **3:** Disadvantage on attack rolls and saving throws
@@ -118,84 +118,84 @@ public class RulesDatabase {
 
 
 
-    private final static String force = """
+    private final static String FORCE = """
                 A raw form of magical energy that hits like a brick wall.
                 - Immune: 1
                 - Resistant: 0
                 - Vulnerable: 0
                 """;
 
-    private final static String radiant = """
+    private final static String RADIANT = """
                 A divine smiting from the highest heavens.
                 - Immune: 2
                 - Resistant: 6
                 - Vulnerable: 2
                 """;
 
-    private final static String thunder = """
+    private final static String THUNDER = """
                 Percussive noise that is loud enough to cause damage.
                 - Immune: 4
                 - Resistant: 25
                 - Vulnerable: 2
                 """;
 
-    private final static String psychic = """
+    private final static String PSYCHIC = """
                 Telepathic abilities inflicting massive damage to the mind.
                 - Immune: 22
                 - Resistant: 4
                 - Vulnerable: 0
                 """;
 
-    private final static String acid = """
+    private final static String ACID = """
                 Intense burning and stinging that can corrode if strong enough.
                 - Immune: 26
                 - Resistant: 32
                 - Vulnerable: 0
                 """;
 
-    private final static String necrotic = """
-                The destruction of ‘life force'. Death, decay, rotting, and corruption.
+    private final static String NECROTIC = """
+                The destruction of 'life force'. Death, decay, rotting, and corruption.
                 - Immune: 27
                 - Resistant: 30
                 - Vulnerable: 0
                 """;
 
-    private final static String lightning = """
+    private final static String LIGHTNING = """
                 Lightning damage is caused by a high voltage of electricity.
                 - Immune: 26
                 - Resistant: 87
                 - Vulnerable: 0
                 """;
 
-    private final static String cold = """
+    private final static String COLD = """
                 Frigid pain leading to potential numbness caused by temperatures approaching zero Kelvin.
                 - Immune: 30
                 - Resistant: 115
                 - Vulnerable: 4
                 """;
 
-    private final static String fire = """
+    private final static String FIRE = """
                 Fire is hot, and can burn your skin if it comes in contact.
                 - Immune: 68
                 - Resistant: 87
                 - Vulnerable: 13
                 """;
 
-    private final static String  poison= """
+    private final static String  POISON= """
                 A toxic substance that is “ingested” and causes harm to the body.
                 - Immune: 207
                 - Resistant: 16
                 - Vulnerable: 0
                 """;
 
-    private final static String  nonMagicMelee= """
+    private final static String  NON_MAGIC_MELEE= """
                 Physical harm caused by a non magical weapon.
                 - Immune: 46
                 - Resistant: 189
                 - Vulnerable: 5
                 """;
 
-    private final static String  magicMelee= """
+    private final static String  MAGIC_MELEE= """
                 Physical harm caused by a magical weapon.
                 - Immune: 0
                 - Resistant: 0
@@ -208,18 +208,18 @@ public class RulesDatabase {
         EmbedBuilder damageEmbed = new EmbedBuilder();
         damageEmbed.setTitle("DND 5e Damage Types");
 
-        damageEmbed.addField("**Magic Weapons**", magicMelee, true);
-        damageEmbed.addField("**Force**", force, true);
-        damageEmbed.addField("**Radiant**", radiant, true);
-        damageEmbed.addField("**Thunder**", thunder, true);
-        damageEmbed.addField("**Psychic**", psychic, true);
-        damageEmbed.addField("**Acid**", acid, true);
-        damageEmbed.addField("**Necrotic**", necrotic, true);
-        damageEmbed.addField("**Lightning**", lightning, true);
-        damageEmbed.addField("**Cold**", cold, true);
-        damageEmbed.addField("**Fire**", fire, true);
-        damageEmbed.addField("**Poision**", poison, true);
-        damageEmbed.addField("**Non-Magic Melee**", nonMagicMelee, true);
+        damageEmbed.addField("**Magic Weapons**", MAGIC_MELEE, true);
+        damageEmbed.addField("**Force**", FORCE, true);
+        damageEmbed.addField("**Radiant**", RADIANT, true);
+        damageEmbed.addField("**Thunder**", THUNDER, true);
+        damageEmbed.addField("**Psychic**", PSYCHIC, true);
+        damageEmbed.addField("**Acid**", ACID, true);
+        damageEmbed.addField("**Necrotic**", NECROTIC, true);
+        damageEmbed.addField("**Lightning**", LIGHTNING, true);
+        damageEmbed.addField("**Cold**", COLD, true);
+        damageEmbed.addField("**Fire**", FIRE, true);
+        damageEmbed.addField("**Poison**", POISON, true);
+        damageEmbed.addField("**Non-Magic Melee**", NON_MAGIC_MELEE, true);
         damageEmbed.setFooter("Stats come from the following books: XGE, VGM, TCE, PHB, MTF, MM and DMG",
                 "https://i.imgur.com/dZvCR8L.png");
         return damageEmbed;
@@ -232,9 +232,9 @@ public class RulesDatabase {
         coverEmbed.setTitle("DND 5e Cover Rules");
         coverEmbed.setDescription("Walls, trees, creatures, and other obstacles can provide cover during combat, making a target more difficult to harm. A target can benefit from cover only when an attack or other effect originates on the opposite side of the cover.");
 
-        coverEmbed.addField("**Half Cover**", halfCover, false);
-        coverEmbed.addField("**Three Quarters Cover**", threeQuartersCover, false);
-        coverEmbed.addField("**Full Cover**", fullCover, false);
+        coverEmbed.addField("**Half Cover**", HALF_COVER, false);
+        coverEmbed.addField("**Three Quarters Cover**", THREE_QUARTERS_COVER, false);
+        coverEmbed.addField("**Full Cover**", FULL_COVER, false);
         coverEmbed.setFooter("Info comes from the Basic Rules",
                 "https://i.imgur.com/dZvCR8L.png");
         return coverEmbed;
@@ -245,22 +245,21 @@ public class RulesDatabase {
     public static EmbedBuilder getConditionEmbed(){
         EmbedBuilder conditionEmbed = new EmbedBuilder();
         conditionEmbed.setTitle("DND 5e Conditions");
-        conditionEmbed.addField("**Blinded**", blinded, false);
-        conditionEmbed.addField("**Charmed**", charmed, false);
-        conditionEmbed.addField("**Deafened**", deafened, false);
-        conditionEmbed.addField("**Frightened**", frightened, false);
-        conditionEmbed.addField("**Grappled**", grappled, false);
-        conditionEmbed.addField("**Incapacitated**", incapacitated, false);
-        conditionEmbed.addField("**Invisible**", invisible, false);
-        conditionEmbed.addField("**Paralyzed**", paralyzed, false);
-        conditionEmbed.addField("**Petrified**", petrified, false);
-        conditionEmbed.addField("**Poisoned**", poisoned, false);
-        conditionEmbed.addField("**Prone**", prone, false);
-        conditionEmbed.addField("**Restrained**", restrained, false);
-        conditionEmbed.addField("**Stunned**", stunned, false);
-        conditionEmbed.addField("**Unconscious**",unconscious, false);
-        conditionEmbed.addField("**Exhausted**", exhaustion, false);
-
+        conditionEmbed.addField("**Blinded**", BLINDED, false);
+        conditionEmbed.addField("**Charmed**", CHARMED, false);
+        conditionEmbed.addField("**Deafened**", DEAFENED, false);
+        conditionEmbed.addField("**Frightened**", FRIGHTENED, false);
+        conditionEmbed.addField("**Grappled**", GRAPPLED, false);
+        conditionEmbed.addField("**Incapacitated**", INCAPACITATED, false);
+        conditionEmbed.addField("**Invisible**", INVISIBLE, false);
+        conditionEmbed.addField("**Paralyzed**", PARALYZED, false);
+        conditionEmbed.addField("**Petrified**", PETRIFIED, false);
+        conditionEmbed.addField("**Poisoned**", POISONED, false);
+        conditionEmbed.addField("**Prone**", PRONE, false);
+        conditionEmbed.addField("**Restrained**", RESTRAINED, false);
+        conditionEmbed.addField("**Stunned**", STUNNED, false);
+        conditionEmbed.addField("**Unconscious**", UNCONSCIOUS, false);
+        conditionEmbed.addField("**Exhausted**", EXHAUSTION, false);
         conditionEmbed.setFooter("Info comes from the Basic Rules",
                 "https://i.imgur.com/dZvCR8L.png");
 
